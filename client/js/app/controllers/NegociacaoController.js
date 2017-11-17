0b2443aa978056deb612806a77955e8b9972ad02
AlbericoD <a.diasbarreto@icloud.com>
2017-11-17 12:19:30 -0200

    Adicionado metodo apagar em listanegociações, vinculado evento ao botão.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,43 +1,55 @@
-class NegociacaoController {
-    constructor() {
-        let $ = document.querySelector.bind(document);
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
+class NegociacaoController
+{
+    constructor()
+    {
+        let $ = document.querySelector.bind( document );
+        this._inputData = $( '#data' );
+        this._inputQuantidade = $( '#quantidade' );
+        this._inputValor = $( '#valor' );
         this._listaNegociacoes = new ListaNegociacoes();
-        this._nview = new NegociacaoView($('#negociacaoView'));
+        this._nview = new NegociacaoView( $( '#negociacaoView' ) );
 
-        this._nview.update(this._listaNegociacoes);
+        this._nview.update( this._listaNegociacoes );
         this._mensagem = new Mensagem();
 
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagemView = new MensagemView( $( '#mensagemView' ) );
+        this._mensagemView.update( this._mensagem );
     }
-    adiciona(event) {
+    adiciona( event )
+    {
         event.preventDefault();
         let negociacao = this._criaNegociacao();
-        this._listaNegociacoes.adiciona(negociacao);
-        this._nview.update(this._listaNegociacoes);
+        this._listaNegociacoes.adiciona( negociacao );
+        this._nview.update( this._listaNegociacoes );
 
         this._mensagem.texto = 'Negociação adicionado com sucesso!';
-        this._mensagemView.update(this._mensagem);
+        this._mensagemView.update( this._mensagem );
 
 
         this._limpaFormulario();
 
         //console.log(this._listaNegociacoes.negociacoes);
     }
-    _criaNegociacao(){
+    _criaNegociacao()
+    {
         return new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
+            DateHelper.textoParaData( this._inputData.value ),
             this._inputQuantidade.value,
             this._inputValor.value
         );
     }
-    _limpaFormulario() {
+    _limpaFormulario()
+    {
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
         this._inputData.focus();
+    }
+    apaga(){
+        this._listaNegociacoes.esvazia();
+        this._nview.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso!';
+        this._mensagemView.update(this._mensagem);
     }
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,11 +1,19 @@
-class ListaNegociacoes {
-    constructor() {
+class ListaNegociacoes
+{
+    constructor()
+    {
         this._negociacoes = [];
     }
-    adiciona(negociacao){
-        this._negociacoes.push(negociacao);
+    adiciona( negociacao )
+    {
+        this._negociacoes.push( negociacao );
     }
-    get negociacoes(){
-        return [].concat(this._negociacoes);//
+    get negociacoes()
+    {
+        return [].concat( this._negociacoes );
+    }
+    esvazia()
+    {
+        this._negociacoes = [];
     }
 }
